4163f3d2deda32fc180d821810bf465d867c3492
GyeongHwan Hong <hkh0904@naver.com>
2024-01-05 02:50:51 +0900

    Conditional statement

---
+++ C++ Practice/Conditional statement/client/main.cpp
@@ -0,0 +1,87 @@
+
+#include <iostream>
+using namespace std;
+
+//int main(void)
+//{
+//	/* 조건문 */
+//	/* bool타입으로 결과를 판단 가능한 무언가(조건식) */
+//
+//	int		iData = 11;
+//
+//	if (100 < iData)
+//	{
+//		cout << "True" << endl;
+//	}
+//	else if (10 < iData <= 100)
+//	{
+//		cout << "11~100" << endl;
+//	}
+//	else 
+//	{
+//		cout << "False" << endl;
+//	}
+//}
+
+//int main(void)
+//{
+//	// switch -case 조건문
+//
+//	int		iData = 10;
+//
+//	//switch (iData)
+//	//{
+//	//case 0:
+//	//	// 수행을 해야 할 기능
+//	//	cout << "0입니다..." << endl;
+//	//	break;	// break 없다면 아래의 모든 case를 실행함
+//
+//	//case 5: [[__fallthrough]];
+//	//case 7: [[__fallthrough]];
+//	//case 10: [[__fallthrough]];
+//	//case 100:
+//	//	cout << "100입니다..." << endl;
+//	//	break;
+//	//}
+//
+//	switch (int iData_A = 10; iData)
+//	{
+//		case 0:
+//			iData_A = 20;
+//			cout << "0입니다.." << endl;
+//			break;
+//
+//		case 5:
+//			cout << "5입니다.." << endl;
+//			break;
+//
+//		default:
+//			cout << "0이나 5가 아닌 값입니다." << endl;
+//			break;
+//	}
+//	
+//	if (int iData_A = 10; iData)
+//	{
+//		cout << "왜 나는 안되지" << endl;
+//	}
+//
+//}
+
+
+// ====================================================
+
+int main(void)
+{
+	// 반복문
+	// (초기값, 조건, 증감)
+
+	int		i, j, k, l = 0;
+
+	for (i = 0; i < 5; ++i)
+	{
+		cout << i << endl;
+	}
+
+	cout << "완료되었습니다." << endl;
+
+}
